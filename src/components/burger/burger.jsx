24bf887e38e0d10709dcd6918a68be842c19d8d1
DeliriousEC <m-trankov@mail.ru>
2024-04-14 22:13:49 +0300

    Поправил выход

--- src/components/burger/burger.jsx
+++ src/components/burger/burger.jsx
@@ -23,19 +23,19 @@ export const Burger = () => {
           <S.MenuList>
             <Link to="/">
               <S.MenuItem>
-                <S.MenuLink href="/">Главное</S.MenuLink>
+                <S.MenuLink>Главное</S.MenuLink>
               </S.MenuItem>
             </Link>
             <Link to="/favorites">
               <S.MenuItem>
-                <S.MenuLink href="/favorites">Мой плейлист</S.MenuLink>
+                <S.MenuLink>Мой плейлист</S.MenuLink>
               </S.MenuItem>
             </Link>
-            <Link>
+            {/* <Link> */}
               <S.MenuItem onClick={handleLogout} >
-                <S.MenuLink href="../Auth">Выйти</S.MenuLink>
+                <S.MenuLink>Выйти</S.MenuLink>
               </S.MenuItem>
-            </Link>
+            {/* </Link> */}
           </S.MenuList>
         </S.NavMenu>
       )}
